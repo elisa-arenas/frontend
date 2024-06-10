ba2e512247530642dbcfbf80ab35db717e27894f
Bernardo <bernardojavier18@gmail.com>
2024-06-10 15:45:28 -0400

    Working search all, bugged search 1

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-const users_template = document.querySelector(`#result-template`)
-console.log(users_template)
-const user_results = document.querySelector(`#user-results`)
-
-const users = await fetch(`http://localhost:8080/api/employees`)
-.then((response) => response.json())
-console.log(users)
-
-for (let user of users) {
-    const user_elems = users_template.content.cloneNode(true)
-    const name = user_elems.querySelector(`h2`)
-    name.replaceWith(`<h2>${user["fullName"]}</h2>`)
-    for (const key in user) {
-        const slot = user_elems.querySelector(`slot[id="${key}"]`)
-        if (slot) {
-            slot.replaceWith(user[key])
+async function populateUsers() {
+    const users_template = document.querySelector(`#result-template`)
+    console.log(users_template)
+    const user_results = document.querySelector(`#user-results`)
+    
+    const users = await fetch(`http://localhost:8080/api/employees`)
+    .then((response) => response.json())
+    console.log(users)
+    
+    for (let user of users) {
+        const user_elems = users_template.content.cloneNode(true)
+        const name = user_elems.querySelector(`h2`)
+        name.innerHTML = user["fullName"]
+        for (const key in user) {
+            const slot = user_elems.querySelector(`slot[id="${key}"]`)
+            if (slot) {
+                slot.replaceWith(user[key])
+            }
         }
+    
+        user_results.prepend(user_elems)
+    
+    }
+}
+
+async function getUser(id) {
+    const users_template = document.querySelector(`#result-template`)
+    console.log(users_template)
+    const user_results = document.querySelector(`#user-results`)
+    
+    const users = await fetch(`http://localhost:8080/api/employees/${id}`)
+    .then((response) => response.json())
+    console.log(users)
+    console.log(id)
+    
+    for (let user of users) {
+        const user_elems = users_template.content.cloneNode(true)
+        const name = user_elems.querySelector(`h2`)
+        name.innerHTML = user["fullName"]
+        for (const key in user) {
+            const slot = user_elems.querySelector(`slot[id="${key}"]`)
+            if (slot) {
+                slot.replaceWith(user[key])
+            }
+        }
+    
+        user_results.prepend(user_elems)
+    
     }
 
-    user_results.prepend(user_elems)
-
 }
+
+const allUsersButton = document.querySelector(`#allUsers`)
+console.log(allUsersButton)
+allUsersButton.addEventListener("click", populateUsers)
+
+const searchButton = document.querySelector(`#oneUser`)
+const searchBar = document.querySelector(`#search-bar`)
+console.log(searchBar.value)
+searchButton.addEventListener("click", getUser(searchBar.value))
 
 
