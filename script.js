63e23170b3399e86e18414b49a7a84c2b88a3d10
Bernardo <bernardojavier18@gmail.com>
2024-06-10 15:19:32 -0400

    Almost full functional frontend for GET

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const users_template = document.querySelector(`#result-template`)
 console.log(users_template)
 const user_results = document.querySelector(`#user-results`)
 
-for (let i=0; i<3; i++) {
+const users = await fetch(`http://localhost:8080/api/employees`)
+.then((response) => response.json())
+console.log(users)
+
+for (let user of users) {
     const user_elems = users_template.content.cloneNode(true)
-    const slot = user_elems.querySelector(`h2`)
-    if(slot) {
-        slot.replaceWith(`Username: ${i} ~`)
+    const name = user_elems.querySelector(`h2`)
+    name.replaceWith(`<h2>${user["fullName"]}</h2>`)
+    for (const key in user) {
+        const slot = user_elems.querySelector(`slot[id="${key}"]`)
+        if (slot) {
+            slot.replaceWith(user[key])
+        }
     }
 
     user_results.prepend(user_elems)
